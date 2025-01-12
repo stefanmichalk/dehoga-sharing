371cde1f6ade756cb1966b15ef4a587f9c959254
stefanmichalk <stefan@s-michalk.de>
2025-01-12 02:14:34 +0100

    wip

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,6 +1,8 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
 import { CommonModule } from '@angular/common';
-import { RouterOutlet } from '@angular/router';
+import { Router, NavigationEnd, RouterOutlet } from '@angular/router';
+import { Meta, Title } from '@angular/platform-browser';
+import { filter } from 'rxjs';
 import { HeaderComponent } from './components/header/header.component';
 import { FooterComponent } from './components/footer/footer.component';
 
@@ -11,6 +13,28 @@ import { FooterComponent } from './components/footer/footer.component';
   templateUrl: './app.component.html',
   styleUrls: ['./app.component.scss']
 })
-export class AppComponent {
+export class AppComponent implements OnInit {
   currentYear = new Date().getFullYear();
+
+  constructor(
+    private router: Router,
+    private meta: Meta,
+    private title: Title
+  ) {}
+
+  ngOnInit() {
+    // Subscribe to router events
+    this.router.events.pipe(
+      filter(event => event instanceof NavigationEnd)
+    ).subscribe((event: any) => {
+      // Reset meta tags when navigating to home page
+      if (event.url === '/') {
+        this.title.setTitle('damit Alle gewinnen');
+        this.meta.updateTag({ name: 'description', content: 'damit Alle gewinnen' });
+        this.meta.updateTag({ property: 'og:title', content: 'damit Alle gewinnen' });
+        this.meta.updateTag({ property: 'og:description', content: 'damit Alle gewinnen' });
+        this.meta.removeTag("property='og:image'");
+      }
+    });
+  }
 }
